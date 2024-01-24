fa4b65e6b13f9331d1d911bd5bd2a6cbc2d1bf4e
Sahak <sahak.keshishyan.dev@gmail.com>
2024-01-24 07:40:36 +0400

    create api-s in backend

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  console.log("asd", asd);
+
+  return (
+    <>
+      <div>Starting...</div>
+    </>
+  );
+}
+
+export default App;
